2e76c93c097b462664256fe7540351e4ec28aedc
Jonathan <kakashi169@gmail.com>
2019-05-30 19:38:49 +0000

    made skeleton for project-details along with future components slashed out; cannot test yet

---
+++ src/components/project-details.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class ProjectDetails extends React.Component {
+    render() {
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <img src="https://bit.ly/2WxCQpY" className="col-3" alt="Project Image" />
+                    <div className="col-5">
+                        <div className="col">Flower Garden</div>
+                        <div className="col">By: Pugnelius McPugpug</div>
+                        <div className="col">Spicy jalapeno bacon ipsum dolor amet alcatra flank rump fatback pancetta porchetta pig swine pork chop bresaola drumstick salami t-bone ground round meatloaf. Pork loin alcatra cow spare ribs. Tenderloin spare ribs pig strip steak alcatra salami brisket chicken shankle tri-tip. Ribeye shank salami, capicola andouille doner beef ribs jowl ham kielbasa biltong boudin. Cupim short ribs fatback pork chop, doner corned beef ball tip turkey sausage kielbasa.</div>
+                    </div>
+                    {/* <Timeline /> */}
+                </div>
+            </div>
+        </div>
+    }
+}
