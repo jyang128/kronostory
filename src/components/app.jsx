8e2892eb83fd2663ed1f12ab107f6a750023e49d
Jonathan <kakashi169@gmail.com>
2019-05-30 22:29:22 +0000

    Added skeleton for project details page. Using placeholder for Products Used. put where timeline and products used components should go

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import ProjectCatalog from './project-catalog';
+import ProjectDetails from './project-details';
 
 export default class App extends React.Component{
     constructor(props){
@@ -26,6 +27,7 @@ export default class App extends React.Component{
             <div className="container">
                 <Header title="Progress Tracker" />
                 <ProjectCatalog />
+                <ProjectDetails />
             </div>
         );
     }
--- src/components/project-details.jsx
+++ src/components/project-details.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 
 export default class ProjectDetails extends React.Component {
     render() {
-        <div>
-            <div className="container-fluid">
-                <div className="row">
-                    <img src="https://bit.ly/2WxCQpY" className="col-3" alt="Project Image" />
-                    <div className="col-5">
-                        <div className="col">Flower Garden</div>
-                        <div className="col">By: Pugnelius McPugpug</div>
-                        <div className="col">Spicy jalapeno bacon ipsum dolor amet alcatra flank rump fatback pancetta porchetta pig swine pork chop bresaola drumstick salami t-bone ground round meatloaf. Pork loin alcatra cow spare ribs. Tenderloin spare ribs pig strip steak alcatra salami brisket chicken shankle tri-tip. Ribeye shank salami, capicola andouille doner beef ribs jowl ham kielbasa biltong boudin. Cupim short ribs fatback pork chop, doner corned beef ball tip turkey sausage kielbasa.</div>
+        return (
+            <div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <img src="https://bit.ly/2WxCQpY" className="col-3" alt="Project Image" />
+                        <div className="col-5">
+                            <h3 className="col">Flower Garden</h3>
+                            <div className="col font-weight-light">By: Pugnelius McPugpug</div>
+                            <div className="col mt-3">Spicy jalapeno bacon ipsum dolor amet alcatra flank rump fatback pancetta porchetta pig swine pork chop bresaola drumstick salami t-bone ground round meatloaf. Pork loin alcatra cow spare ribs. Tenderloin spare ribs pig strip steak alcatra salami brisket chicken shankle tri-tip. Ribeye shank salami, capicola andouille doner beef ribs jowl ham kielbasa biltong boudin. Cupim short ribs fatback pork chop, doner corned beef ball tip turkey sausage kielbasa.</div>
+                        </div>
+                        <div className="productsUsedPlaceholder col-4 text-white bg-secondary">
+                            Things I used
+                        </div>
+                        {/* <ProductsUsed /> */}
                     </div>
-                    {/* <Timeline /> */}
                 </div>
+                {/* <Timeline /> */}
             </div>
-        </div>
+        )
     }
 }
