e1d31d7ab97a5938243e3cfb68c890c328f53e35
Jonathan <kakashi169@gmail.com>
2019-06-01 21:06:46 +0000

    created login component. No functionality yet

--- src/components/user-login.jsx
+++ src/components/user-login.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 
 export default class UserLogin extends React.Component {
     render() {
-        return 
+        return (
+            <div className="text-center mt-5 col-4 offset-4">
+                <form className="form-signin">
+                    <h1 className="font-weight-light text-primary">KronoStory</h1>
+                    <h1 className="h3 mb-3 font-weight-light">Please sign in</h1>
+                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="text" autoFocus />
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required="password" />
+                    <div className="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me" /> Remember me
+                        </label>
+                    </div>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                    <p className="mt-5 mb-2 text-muted">Don't have an account?</p>
+                    <button className="btn btn-lg btn-outline-primary btn-block">Sign Up</button>
+                </form>
+            </div>
+        )
     }
 }
+
