01f241162aa6f59bc59cb6c1769063e7b5fa25cc
paulinawebdev <paulinawebdev@gmail.com>
2019-06-03 18:31:20 -0700

    changed create-project.php

--- server/public/api/uploads/create-project.php
+++ server/public/api/uploads/create-project.php
@@ -1,54 +1,52 @@
 <?php
-
+    //require_once '../db_connection.php';
     require_once '../functions.php';
 
     set_exception_handler("error_handler");
 
-    startup();
+    //r$output = ["error"=> "none", "upload"=>"Success"];
 
-    
+    $userId = $_POST["user-id"];
+    $userId = json_decode($userId, true);
 
-    $output = ["error"=> "none", "upload"=>"Success"];
+    $target_dir = 'image-uploads/' . $userId . '/';
+    $target_file = $target_dir . $_FILES["main-proj-img"]["name"];
 
-    $target_dir = 'image-uploads/';
-    $target_file = $target_dir . $_FILES["imageToUpload"]["name"];
-
-    if (file_exists($target_file)) {
-        $output["error"] = 'File already exists';
-        $output["upload"] = 'Fail';
-        echo json_encode($output);
-        return;
+    if (is_dir($target_dir)) {
+        if (file_exists($target_file)) {
+            $output["error"] = 'File already exists';
+            $output["upload"] = 'Fail';
+            echo json_encode($output);
+            return;
+        }
+    } else {
+        mkdir($target_dir);
     }
 
     if (isset($_POST["hasUpload"])){
         
-        $file = $_FILES["imageToUpload"];
+        $file = $_FILES["main-proj-img"];
         $pathExtension = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
 
         if($file["size"] > 3000000) {
             $output["error"] = 'The file is too large';
             $output["upload"] = 'Fail';
-            print $output["error"];
 
         } elseif(($pathExtension !== 'jpg') && ($pathExtension !== 'png') && ($pathExtension !== 'gif') && ($pathExtension !== 'jpeg')) {
 
             $output["error"] = 'Wrong File Name. Must be jpg, png, jpeg, or gif';
             $output["upload"] = 'Fail';
-            print $output["error"];
+            //throw new Exception('Wrong file name');
 
         } else {
             
-            if(move_uploaded_file($_FILES["imageToUpload"]["tmp_name"], $target_file)) {
+            if(move_uploaded_file($_FILES["main-proj-img"]["tmp_name"], $target_file)) {
                 $output["filepath"] = stripslashes($target_file);
-                $output["msg"] = "The file " . $_FILES["imageToUpload"]["name"] . " has been uploaded.";
+                $output["msg"] = "The file " . $_FILES["main-proj-img"]["name"] . " has been uploaded.";
             }
         }
     }
 
-    print_r("the file: " . $_FILES);
-    print("the file: " . $_FILES);
-    var_dump( $_FILES);
-    echo json_encode($_FILES);
 
     echo json_encode($output);
 
