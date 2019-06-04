aff00f9d3a575d21b0188fc424c3acab2bcb005d
Jonathan <kakashi169@gmail.com>
2019-06-04 21:39:48 +0000

    added ability to input and receive data on one user

---
+++ server/public/api/login.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once './db_connection.php';
+require_once './functions.php';
+
+set_exception_handler("error_handler");
+
+startup();
+
+if (!$conn){
+  throw new Exception('there is an error' . mysqli_connect_error());
+}
+
+$username = $_GET['username'];
+
+$query = "SELECT * FROM `user` 
+    WHERE `username` = '{$username}'";
+
+if ($result = mysqli_query($conn, $query)) {
+    $numRows = mysqli_num_rows($result);
+} else {
+    throw new Exception('there is an error' . mysqli_connect_error());
+}
+
+if ($numRows === 0) {
+    throw new Exception("Username is incorrect/does not exist");
+}
+
+$output = [];
+
+while ($row = mysqli_fetch_assoc($result)) {   
+    $output[] = $row;
+}
+
+$json_output = json_encode($output);
+print $json_output;
+
+?>
