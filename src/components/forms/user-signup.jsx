d2ad94ec9b3d57187ce5615624584a2eff266ea5
Claudius King <tezcatlipoca7123@gmail.com>
2019-06-05 07:35:14 -0700

    Added setstate onchange

--- src/components/forms/user-signup.jsx
+++ src/components/forms/user-signup.jsx
@@ -3,7 +3,28 @@ import React from 'react';
 export default class UserSignup extends React.Component {
   constructor(props){
     super(props);
+    this.state={email:"",password:"",repassword:"",firstName:"",lastName:""};
     this.signupHandler=this.signupHandler.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+  onChangeHandler(event){
+    switch(event.target.id){
+      case "inputEmail":
+        this.setState({email: event.target.value});
+        break;
+      case "inputPassword":
+        this.setState({password: event.target.value});
+        break;
+      case "reEnter":
+        this.setState({repassword: event.target.value});
+        break;
+      case "firstName":
+        this.setState({firstName: event.target.value});
+        break;
+      case "lastName":
+        this.setState({lastName: event.target.value});
+        break;
+    }
   }
     signupHandler(event){
       event.preventDefault();
@@ -16,15 +37,15 @@ export default class UserSignup extends React.Component {
                     <h1 className="font-weight-light text-primary">KronoStory</h1>
                     <h1 className="h3 mb-3 font-weight-light">Sign Up</h1>
                     <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" className="form-control mb-2" placeholder="Email address" required="text" autoFocus />
+                    <input type="email" id="inputEmail" className="form-control mb-2" placeholder="Email address" required="text" onChange={this.onChangeHandler} />
                     <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control mb-2" placeholder="Password" required="password" />
+                    <input type="password" id="inputPassword" className="form-control mb-2" placeholder="Password" required="password" onChange={this.onChangeHandler} />
                     <label htmlFor="inputPassword" className="sr-only">Re-enter Password</label>
-                    <input type="password" id="reEnter" className="form-control mb-2" placeholder="Re-enter Password" required="password" />
+                    <input type="password" id="reEnter" className="form-control mb-2" placeholder="Re-enter Password" required="password" onChange={this.onChangeHandler} />
                     <label htmlFor="inputPassword" className="sr-only">First Name</label>
-                    <input type="text" id="firstName" className="form-control mb-2" placeholder="First Name" />
+                    <input type="text" id="firstName" className="form-control mb-2" placeholder="First Name" onChange={this.onChangeHandler} />
                     <label htmlFor="inputPassword" className="sr-only">Last Name</label>
-                    <input type="text" id="lastName" className="form-control mb-2" placeholder="Last Name" />
+                    <input type="text" id="lastName" className="form-control mb-2" placeholder="Last Name" onChange={this.onChangeHandler} />
                     <div className="checkbox">
                     </div>
                     <button className="btn btn-lg btn-primary btn-block" onClick={this.signupHandler}>Sign Up</button>
