41d0c6d7e5c9dd87d7914a2115a0980f46eeef90
jyang128 <jessicayang128@gmail.com>
2019-06-06 20:16:39 +0000

    reverted modal to bootstrap

--- src/components/project/timeline-entry-form.jsx
+++ src/components/project/timeline-entry-form.jsx
@@ -29,48 +29,36 @@ export default class TimelineEntryForm extends React.Component {
     }
     render() {
         return (
-            <Modal isOpen={this.props.modal} toggle={this.props.toggle} size="lg">
-            <ModalHeader toggle={this.props.toggle}></ModalHeader>
-            <ModalBody>
-            <Col xs={{size: 8, offset: 2}} className="text-center align-self-center">
-                <h2>New Timeline Entry</h2>
-                <Form className="my-4" onSubmit={event => this.handleSubmitPress(event)}>
-                    <FormGroup>
-                        <Label>Give it a short title</Label>
-                        <Input 
-                            type="text" 
-                            id="inputTitle" 
-                            value={this.state.title}
-                            className="form-control" 
-                            placeholder="Enter Title"
-                            onChange={this.onChangeHandler}
-                        />
-                        <small className="form-text text-muted">Make it sound cool! Maximum 60 characters.</small>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>Summary</Label>
-                        <Input 
-                            type="textarea" 
-                            id="inputDescription" 
-                            value={this.state.description}
-                            className="form-control" 
-                            placeholder="Summary of the project."
-                            onChange={this.onChangeHandler}
-                        />
-                        <small className="form-text text-muted">A short description that says it all. Maximum 140 characters.</small>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>Main Image</Label>
-                        <Input 
+            <React.Fragment>
+                <div className="col-8 offset-2 text-center align-self-center">
+                    <form className="my-4" onSubmit={event => this.handleSubmitPress(event)}>
+                    <h2>New Timeline Entry</h2>
+                    <div className="form-group">
+                        <label>Give it a short title</label>
+                        <input type="text" 
+                        id="inputTitle"
+                        value={this.state.title}className="form-control" placeholder="Enter Title"
+                        onChange={this.onChangeHandler}
+                    />
+                    </div>
+                    <div className="form-group">
+                        <label>Description</label>
+                        <textarea
+                        id="inputDescription"
+                        value={this.state.description}
+                        className="form-control" placeholder="Summary of the project."
+                        onChange={this.onChangeHandler}></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label>Main Image</label>
+                        <input 
                             type="file" 
-                            className="form-control-file"
-                        />
-                    </FormGroup>
-                    <Button type="submit" color="primary" className="mr-2" >Submit</Button>
-                </Form>
-            </Col>
-            </ModalBody>
-            </Modal>
+                            className="form-control-file"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary mr-2" >Submit</button>
+                    </form>
+            </div>
+            </React.Fragment>
         );
     }
 }
