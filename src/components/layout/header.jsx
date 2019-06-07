a30ffbc56096d81ad3b9e9eaba14a3c2ce3d22a4
Jonathan <kakashi169@gmail.com>
2019-06-07 05:01:35 +0000

    made new branch to try to push to dev

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -16,15 +16,23 @@ export default class Header extends React.Component {
                 </h1>
 
                 {this.props.currentUser 
-                ? <div className="menu-nav align-self-center">
-                    <Link to="/dashboard">
-                        <i className="far fa-user-circle"></i> 
-                        <span>{this.props.currentUser.username}</span>
-                    </Link>
-                </div>
-                :  <div className="menu-nav align-self-center">
-                     <Link to="/user-signup">Sign Up</Link> <Link to="/user-login">Log In</Link>
-                   </div>
+                    ?   <div className="menu-nav align-self-center">
+                            <Link to={{
+                                pathname: '/dashboard',
+                                search: `?user=${this.props.currentUser.id}`,
+                                state: {
+                                    userId: this.props.currentUser.id, 
+                                    username: this.props.currentUser.username, 
+                                    loggedUser: this.props.currentUser
+                                }
+                            }}>
+                                <span className="mr-1">{this.props.currentUser.username}</span>
+                                <i className="far fa-user-circle"></i>  
+                            </Link>
+                        </div>
+                    :  <div className="menu-nav align-self-center">
+                            <Link to="/user-signup">Sign Up</Link> <Link to="/user-login">Log In</Link>
+                        </div>
                 }
             </div>
             
