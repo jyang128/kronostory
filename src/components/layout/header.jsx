456ff7633a7b81338b73f1310584027da43037c9
jyang128 <jessicayang128@gmail.com>
2019-06-12 23:07:48 +0000

    refactor header and project card dropdown behavior, added methods to handle clicks and mouseLeave and scroll events, deleted empty profile.css file, added CSS styling for dropdowns

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -5,23 +5,80 @@ import { Link } from 'react-router-dom';
 export default class Header extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            showDropdownMenu: false
+        }
+        this.toggleMenuNav = this.toggleMenuNav.bind(this);
+        this.mouseLeaveMenuNav = this.mouseLeaveMenuNav.bind(this);
+        this.closeDropdown = this.closeDropdown.bind(this);
     }
-    dashboardHandler(event){
-        event.target.parentElement.className += " d-none";
-    }
-    dropdownHandler(event){
-		if(!event.target.querySelector(".dropdown-low")){
-			return;
-		}
-		if(event.target.querySelector(".dropdown-low").className === "dropdown-low d-none"){
-			event.target.querySelector(".dropdown-low").className = "dropdown-low";
-		}
-		else{
-			event.target.querySelector(".dropdown-low").className += " d-none";
-		}
+    componentDidMount(){
+		document.addEventListener('click', this.closeDropdown)
+		window.addEventListener('scroll', this.closeDropdown)
 	}
+	componentWillUnmount(){
+		document.removeEventListener('click', this.closeDropdown)
+		window.removeEventListener('scroll', this.closeDropdown)
+	}
+    closeDropdown(event){
+        if(event.target.className !== 'username'){
+            this.setState({showDropdownMenu: false})
+        }
+    }
+    toggleMenuNav(){
+        this.setState({showDropdownMenu: !this.state.showDropdownMenu});
+    }
+    mouseLeaveMenuNav(){
+        this.setState({showDropdownMenu: false});
+    }
     render(){
+        let menuNav;
+        let menuNavClass;
+        let menuNavDropdownClass;
+
+        if(this.state.showDropdownMenu){
+            menuNavClass = "menu-nav-dropdown";
+            menuNavDropdownClass = "menu-nav-dropdown-arrow"
+        } else {
+            menuNavClass = "menu-nav-dropdown d-none"
+            menuNavDropdownClass = "menu-nav-dropdown-arrow d-none"
+        }
+        
+        if(this.props.userSeshData.id) {
+            menuNav = (
+                <div className="menu-nav align-self-center" >
+                    <i className="far fa-user-circle"></i> 
+                    <span className="username" onClick={this.toggleMenuNav}>
+                        {this.props.userSeshData.username} &#9663;
+                        <div className={menuNavClass} onMouseLeave={this.mouseLeaveMenuNav}>
+                            <Link to='/user-login' className="dropdown-link" onClick={this.props.logoutHandler}>
+                                logout
+                            </Link>
+                            <Link to={{
+                                pathname: `/${this.props.userSeshData.username}`,
+                                state: {
+                                    userId: this.props.userSeshData.id, 
+                                    username: this.props.userSeshData.username, 
+                                }
+                            }} className="dropdown-link" onClick={this.dashboardHandler}>
+                                dashboard
+                            </Link>
+                        </div>
+                    </span>
+                    <div className={menuNavDropdownClass}></div>
+                </div>
+                );
+        } else {
+            menuNav = (
+                <div className="menu-nav align-self-center">
+                    <Link to="/user-signup">Sign Up</Link>
+                    <Link to="/user-login">Log In</Link>
+                </div>
+                );
+        }
+
         return(
+            <React.Fragment>
             <div className="row d-flex justify-content-between py-2 mx-2">
                 <div className="logo d-flex align-items-center">
                     <div className="logo-icon">
@@ -33,35 +90,9 @@ export default class Header extends React.Component {
                         </Link>
                     </h2>
                 </div>
-                
-                {this.props.userSeshData.id 
-                    ?   <div className="menu-nav align-self-center">
-                            <i className="far fa-user-circle"></i> 
-                            <span className="mr-1" onClick={this.dropdownHandler}>
-                                {this.props.userSeshData.username} &#9663;
-                                <div className="dropdown-low d-none">
-                                    <Link to='/user-login' className="logout dropdown-link" onClick={this.props.logoutHandler}>
-                                        logout
-                                    </Link>
-                                    <Link to={{
-                                        pathname: `/${this.props.userSeshData.username}`,
-                                        state: {
-                                            userId: this.props.userSeshData.id, 
-                                            username: this.props.userSeshData.username, 
-                                        }
-                                    }} className="dashboard dropdown-link" onClick={this.dashboardHandler}>
-                						dashboard
-                					</Link>
-                                </div>
-                            </span>
-                        </div>
-                    :  <div className="menu-nav align-self-center">
-                            <Link to="/user-signup">Sign Up</Link>
-                            <Link to="/user-login">Log In</Link>
-                        </div>
-                }
+                {menuNav}
             </div>
-            
+            </React.Fragment>
         );
     }
 }
--- src/components/project/project-card.jsx
+++ src/components/project/project-card.jsx
@@ -5,47 +5,76 @@ import { Link } from 'react-router-dom';
 export default class ProjectCard extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			showDelete: false
+		}
+		this.handleDelete = this.handleDelete.bind(this);
+		this.toggleDeleteButton = this.toggleDeleteButton.bind(this);
+		this.mouseLeaveDeleteButton = this.mouseLeaveDeleteButton.bind(this);
+		this.closeDropdown = this.closeDropdown.bind(this);
 	}
-	dropdown(event){
-		if(!event.target.querySelector(".delete")){
-			return;
+	componentDidMount(){
+		document.addEventListener('click', this.closeDropdown)
+		window.addEventListener('scroll', this.closeDropdown)
+	}
+	componentWillUnmount(){
+		document.removeEventListener('click', this.closeDropdown)
+		window.removeEventListener('scroll', this.closeDropdown)
+	}
+	closeDropdown(event){
+		if(event.target.className !== 'dots'){
+			this.setState({showDelete: false})
 		}
-		if(event.target.querySelector(".delete").className === "delete d-none"){
-			event.target.querySelector(".delete").className = "delete";
-		}
-		else{
-			event.target.querySelector(".delete").className += " d-none";
-		}
+	}
+	toggleDeleteButton(){
+		this.setState({showDelete: !this.state.showDelete})
+	}
+	mouseLeaveDeleteButton(){
+		this.setState({showDelete: false})
+	}
+	handleDelete(){
+		this.toggleDeleteButton();
+		this.props.delete(this.props.projectData.id);
 	}
 	render(){
 		let deleteButton;
+		let deleteButtonClass;
+		let dropdownClass;
+
+		if(this.state.showDelete){
+			deleteButtonClass = 'delete-button';
+			dropdownClass = 'delete-dropdown-arrow';
+		} else {
+			deleteButtonClass = 'delete-button d-none';
+			dropdownClass = 'delete-dropdown-arrow d-none';
+		}
+
 		if(this.props.userStatus) {
 			if(this.props.userStatus.id === this.props.projectData.user_id){
-				deleteButton = (
-				<div>
-					<div className="dots" onClick={this.dropdown}>
-						...
-					<div className="delete d-none" onClick={ () => {this.props.delete(this.props.projectData.id)} }>
-						delete
+					deleteButton = (
+					<div>
+						<div className="dots" onClick={this.toggleDeleteButton} >
+							...
+							<div className={deleteButtonClass} onClick={this.handleDelete} >
+								delete
+							</div>
+							<div className={dropdownClass}></div>
+						</div>
 					</div>
-					</div>
-				</div>
-				);
-			}
+					);
+			} 
 		} else {
 			deleteButton = null;
 		}
 
 		let primaryImg = this.props.projectData.primary_image;
 
-		//
-
 		const imgStyle = {
 			backgroundImage: 'url(' + (primaryImg ? primaryImg : "/images/placeholder-img.jpg") + ')'
 		};
 		return(
 		<div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
-			<div className="card">
+			<div className="card" onMouseLeave={this.mouseLeaveDeleteButton}>
 				<div className="card-main-img" style={imgStyle}></div>
 				<div className="card-body">
 					<h5 className="card-title">{this.props.projectData.title}</h5>
