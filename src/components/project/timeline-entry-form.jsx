0c979e4a2cce0b0945135ae76472b6b8ea99b587
Claudius King <tezcatlipoca7123@gmail.com>
2019-06-13 14:13:42 -0700

    Added validation for images on the timeline form

--- src/components/project/timeline-entry-form.jsx
+++ src/components/project/timeline-entry-form.jsx
@@ -9,7 +9,9 @@ export default class TimelineEntryForm extends React.Component {
             date: '',
             description: '',
             imageFile: null,
-            fileAttached: false
+            fileAttached: false,
+            mainProjectImageLimit: false,
+            mainProjectImageType: false
         };
         this.onChangeHandler = this.onChangeHandler.bind(this);
         this.fileUploadHandler = this.fileUploadHandler.bind(this);
@@ -17,11 +19,25 @@ export default class TimelineEntryForm extends React.Component {
     }
     fileUploadHandler(event){
         console.log('timeline entry image file: ', event.target.files[0]);
+        this.setState({mainProjectImageLimit: false, mainProjectImageType: false});
         if(event.target.files[0]){
             this.setState({
                 imageFile: event.target.files[0],
                 fileAttached: true
-            }) 
+            },()=>{
+                if(this.state.imageFile.name.length >= 100){
+                    this.setState({mainProjectImageLimit: true});
+                }
+                let fileExtension = this.state.imageFile.name.substr(this.state.imageFile.name.length - 4);
+                switch(fileExtension){
+                    case ".jpg":
+                    case ".png":
+                    case ".gif":
+                        break;
+                    default:
+                        this.setState({mainProjectImageType: true});
+                }
+            });
         } else {
             this.setState({
                 imageFile: '',
@@ -31,8 +47,10 @@ export default class TimelineEntryForm extends React.Component {
     }
     formSubmitHandler(event){
         event.preventDefault();
-        let formData = new FormData(event.target);
-        this.props.createNewEntry(formData);
+        if(!this.state.mainProjectImageType && !this.state.mainProjectImageLimit){
+            let formData = new FormData(event.target);
+            this.props.createNewEntry(formData);
+        }
     }
     onChangeHandler(event){
         switch(event.target.id){
@@ -47,30 +65,36 @@ export default class TimelineEntryForm extends React.Component {
         }
     }
     render() {
+        const mainProjectImageError = <div className="form-error">
+                <p className="text-danger">Main project image has a 100 character limit. Currently, it has {this.state.imageFile ? this.state.imageFile.name.length : "" }</p>
+            </div>;
+        const mainProjectImageTypeError = <div className="form-error">
+                <p className="text-danger">Main project image needs to be of type JPG/PNG/GIF</p>
+            </div>;
         return (
             <div className="col-12 col-md-8 offset-md-2 align-self-center">
                 <form className="my-4" onSubmit={this.formSubmitHandler}>
                 <h2 className="text-center mb-4">New Timeline Entry</h2>
                 <div className="form-group">
                     <label>Give it a short title</label>
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         id="inputTitle"
                         name="inputTitle"
                         value={this.state.title}
-                        className="form-control" 
+                        className="form-control"
                         placeholder="Enter Title"
                         onChange={this.onChangeHandler}
                     />
                 </div>
                 <div className="form-group">
                     <label>Date</label>
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         id="inputDate"
                         name="inputDate"
                         value={this.state.date}
-                        className="form-control" 
+                        className="form-control"
                         placeholder="Enter Date"
                         onChange={this.onChangeHandler}
                     />
@@ -81,7 +105,7 @@ export default class TimelineEntryForm extends React.Component {
                         id="inputDescription"
                         name="inputDescription"
                         value={this.state.description}
-                        className="form-control" 
+                        className="form-control"
                         rows="5"
                         placeholder="Summary of the entry."
                         onChange={this.onChangeHandler}>
@@ -89,17 +113,19 @@ export default class TimelineEntryForm extends React.Component {
                 </div>
                 <div className="form-group">
                     <label>Choose an Image</label>
+                    {this.state.mainProjectImageLimit ? mainProjectImageError : null}
+                    {this.state.mainProjectImageType ? mainProjectImageTypeError : null}
                     <div className="custom-file">
-                        <input 
-                            type="file" 
-                            className="form-control-file" 
+                        <input
+                            type="file"
+                            className="form-control-file"
                             id="entry-img"
                             name="entry-img"
                             onChange={this.fileUploadHandler}
                         />
                         <label className="custom-file-label" htmlFor="entry-img">{this.state.imageFile ? this.state.imageFile.name : "Choose file up to 4MB"}</label>
-                        <input 
-                            id="imgAttached" type="hidden" name="imgAttached" value={this.state.fileAttached} 
+                        <input
+                            id="imgAttached" type="hidden" name="imgAttached" value={this.state.fileAttached}
                         />
                     </div>
                 </div>
