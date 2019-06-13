76539ec49627af3e4e3d1543ad81164bcdde5e8d
Claudius King <tezcatlipoca7123@gmail.com>
2019-06-13 14:28:40 -0700

    Added image validation for all the forms

--- src/components/project/items-used-entry-form.jsx
+++ src/components/project/items-used-entry-form.jsx
@@ -7,18 +7,34 @@ export default class ItemUsedEntryForm extends React.Component {
         this.state = {
             title: '',
             imageFile: null,
-            fileAttached: false
+            fileAttached: false,
+            mainProjectImageLimit: false,
+            mainProjectImageType: false
         };
         this.onChangeHandler = this.onChangeHandler.bind(this);
         this.fileUploadHandler = this.fileUploadHandler.bind(this);
         this.formSubmitHandler = this.formSubmitHandler.bind(this);
     }
     fileUploadHandler(event){
+        this.setState({mainProjectImageLimit: false, mainProjectImageType: false});
         if(event.target.files[0]){
             this.setState({
                 imageFile: event.target.files[0],
                 fileAttached: true
-            }) 
+            },()=>{
+                if(this.state.imageFile.name.length >= 100){
+                    this.setState({mainProjectImageLimit: true});
+                }
+                let fileExtension = this.state.imageFile.name.substr(this.state.imageFile.name.length - 4);
+                switch(fileExtension){
+                    case ".jpg":
+                    case ".png":
+                    case ".gif":
+                        break;
+                    default:
+                        this.setState({mainProjectImageType: true});
+                }
+            });
         } else {
             this.setState({
                 imageFile: '',
@@ -28,25 +44,33 @@ export default class ItemUsedEntryForm extends React.Component {
     }
     formSubmitHandler(event){
         event.preventDefault();
-        let formData = new FormData(event.target);
-        this.props.createNewItemEntry(formData);
+        if(!this.state.mainProjectImageType && !this.state.mainProjectImageLimit){
+            let formData = new FormData(event.target);
+            this.props.createNewItemEntry(formData);
+        }
     }
     onChangeHandler(event){
         this.setState({title: event.target.value});
     }
     render() {
+        const mainProjectImageError = <div className="form-error">
+                <p className="text-danger">Main project image has a 100 character limit. Currently, it has {this.state.imageFile ? this.state.imageFile.name.length : "" }</p>
+            </div>;
+        const mainProjectImageTypeError = <div className="form-error">
+                <p className="text-danger">Main project image needs to be of type JPG/PNG/GIF</p>
+            </div>;
         return (
             <div className="col-12 col-md-8 offset-md-2 align-self-center">
                 <form className="my-4" onSubmit={this.formSubmitHandler}>
                 <h2 className="text-center mb-4">New Item Used</h2>
                 <div className="form-group">
                     <label>Item Name</label>
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         id="inputTitle"
                         name="inputTitle"
                         value={this.state.title}
-                        className="form-control" 
+                        className="form-control"
                         placeholder="Enter Item Name"
                         onChange={this.onChangeHandler}
                     />
@@ -54,17 +78,19 @@ export default class ItemUsedEntryForm extends React.Component {
 
                 <div className="form-group">
                     <label>Choose an Image</label>
+                    {this.state.mainProjectImageLimit ? mainProjectImageError : null}
+                    {this.state.mainProjectImageType ? mainProjectImageTypeError : null}
                     <div className="custom-file">
-                        <input 
-                            type="file" 
-                            className="form-control-file" 
+                        <input
+                            type="file"
+                            className="form-control-file"
                             id="entry-img"
                             name="entry-img"
                             onChange={this.fileUploadHandler}
                         />
                         <label className="custom-file-label" htmlFor="entry-img">{this.state.imageFile ? this.state.imageFile.name : "Choose file up to 4MB"}</label>
-                        <input 
-                            id="imgAttached" type="hidden" name="imgAttached" value={this.state.fileAttached} 
+                        <input
+                            id="imgAttached" type="hidden" name="imgAttached" value={this.state.fileAttached}
                         />
                         <small className="text-muted">Recommended: Square size images</small>
                     </div>
